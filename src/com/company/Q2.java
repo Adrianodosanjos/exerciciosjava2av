007b0d489540f208db7cca99d3b04f7a68140d4d
ADRIANO DOS ANJOS BARRETO <04045219@sempreunama.com.br>
2019-11-05 09:49:41 -0300

    Initial commit

---
+++ src/com/company/Q2.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static String calcSemana(int codigoSemana) {
+        switch(codigoSemana){
+            case 1:
+                return "Domingo";
+            case 2:
+                return "Segunda-Feira";
+            case 3:
+                return "Terça-Feira";
+            case 4:
+                return "Quarta-Feira";
+            case 5:
+                return "Quinta-Feira";
+            case 6:
+                return "Sexta-Feira";
+            case 7:
+                return "Sábado";
+
+            default:
+                return "";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Dados
+        System.out.println("Informe o 'código' da semana: ");
+        int codigoSemana = sc.nextInt();
+        //Verificador do dia
+        String semana = calcSemana(codigoSemana);
+       //Saída de dados
+        System.out.println("O dia da semana é " + semana);
+    }
+}
